ef90abfa879885f89991f031678e912815950f9c
mustafa1133 <mustafa1133@hotmail.com>
2019-04-25 18:35:30 -0400

    First commit

---
+++ Lab2/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Car
+    {
+        public int Year { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        public bool IsDrivable { get; set; }
+
+        public double Price { get; set; }
+
+
+
+        public Car(int iyear, string iman, string imodel, bool idriveable, double iprice)
+        {
+            Year = iyear;
+            Manufacturer = iman;
+            Model = imodel;
+            IsDrivable = idriveable;
+            Price = iprice;
+        }
+
+        public string TellAboutSelf()
+        {
+        
+          return $"Year: {Year}\n Manufacturer: {Manufacturer}\n Model: {Model}\n Price: {Price}\n Price: {Price}\n";
+
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car(1993,"Japan" ,"honda", true, 1700);
+            Car c2 = new Car(1994, "Canada" ,"Ford", false, 1800);
+            Car c3 = new Car(1995, "USA" ,"GMC", true, 1900);
+            Car c4 = new Car(1996, "Germany" ,"Jetta", false, 2700);
+            Car c5 = new Car(1997, "Italy" ,"Porche", true, 3700);
+
+            Console.WriteLine( c1.TellAboutSelf());
+            Console.WriteLine(c2.TellAboutSelf());
+            Console.WriteLine(c3.TellAboutSelf());
+            Console.WriteLine(c4.TellAboutSelf());
+            Console.WriteLine(c5.TellAboutSelf());
+
+        }
+    }
+}
